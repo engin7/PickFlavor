8a94b0e1ef381614eb8e11d058084d7a4605d708
Engin <kuk.engin@gmail.com>
2021-10-19 18:36:48 +0300

    dependecies fixed

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jdg/MBProgressHUD", from: "1.1.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("4.8.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PickFlavor",
-            dependencies: []),
+            dependencies: ["Alamofire","MBProgressHUD"]),
         .testTarget(
             name: "PickFlavorTests",
             dependencies: ["PickFlavor"]),
